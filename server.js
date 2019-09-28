96b134e17ef0fd88f9aed8a51d1ef73be236462b
Keith Kovach <kovachks90@gmail.com>
2019-09-28 12:05:32 -0400

    adding wildcard route

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static('client/build'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+});
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
